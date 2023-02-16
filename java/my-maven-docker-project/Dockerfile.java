a21368d50d876b966e00a54f64443a91b775731b
rajeevreddy <rajeev1026@gmail.com>
2023-02-16 16:34:59 +0000

    added new project

--- java/my-maven-docker-project/Dockerfile.java
+++ java/my-maven-docker-project/Dockerfile.java
@@ -1,4 +1,4 @@
 FROM adoptopenjdk/maven-openjdk11 
-ADD target/my-maven-docker-project.jar my-maven-docker-project.jar
-ENTRYPOINT ["java", "-jar","my-maven-docker-project.jar"]
+ADD target/my-maven-docker-project.jar /my-maven-docker-project.jar
+CMD ["java", "-jar","my-maven-docker-project.jar"]
 EXPOSE 8080
---
+++ tutorial/tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/preethimbbs22/dockerlab/lab1/tutorial/tutorial/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
